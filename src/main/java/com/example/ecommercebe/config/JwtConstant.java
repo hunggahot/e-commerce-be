a62a406c9c9a4c846d47553d7b8e343678d82132
hunggahot <hunggahot@gmail.com>
2023-08-13 16:32:57 +0700

    Jwt Security Config

---
+++ src/main/java/com/example/ecommercebe/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.example.ecommercebe.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "adjakjdasjdsakdjaskdjasdjsakdjsadjasaswqidpqiwjdasndqwgfqcfSc";
+    public static final String JWT_HEADER = "Authorization";
+}
